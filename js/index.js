02e6b0da87b6f1fee9a18798f3eec10e367c3116
lemontree <gonelove.guo@gmail.com>
2017-05-22 00:20:12 +0800

    完成

--- js/index.js
+++ js/index.js
@@ -51,17 +51,95 @@ $(function() {
               'transition': '0.3s',
               'transform': 'rotate(360deg)'
             });
-            $('.column img.primary').animate({
+            $('.column img.sport').animate({
               'scale': 1.5
-            }, 'fast', function() {
-              
+            }, 'normal', function() {
+              $(this).css({
+                'transition': '0.3s',
+                'transform': 'rotate(360deg)'
+              });
+              $('.column img.edition').animate({
+                'scale': 1.5
+              }, 'normal', function() {
+                $(this).css({
+                  'transition': '0.3s',
+                  'transform': 'rotate(360deg)'
+                });
+              });              
             });
           });
+          $('.column h4.primary').animate({
+            'scale': 1.5  
+          }, 'fast', function(){
+            $(this).css({
+              'transition': '0.3s',
+              'transform': 'scale(1.5)'
+            });
+            $('.column h4.sport').animate({
+              'scale': 1.5
+            }, 'normal', function() {
+              $(this).css({
+                'transition': '0.3s',
+                'transform': 'scale(1.5)'
+              });
+              $('.column h4.edition').animate({
+                'scale': 1.5
+              }, 'normal', function() {
+                $(this).css({
+                  'transition': '0.3s',
+                  'transform': 'scale(1.5)'
+                });
+              });              
+            });
+          });          
           break;
       }
     },
-    'onleave': function(index, nextIndex, direction) {
-      console.log('滚动前', index);  
+    'onLeave': function(index, nextIndex, direction) {
+      console.log('滚动前', index ,nextIndex);
+      switch(index) {
+        case 1: 
+          setTimeout(function() {            
+            $('.section1 h1').css({
+              'transform': 'scale(1)',
+              'transition': '0.4s'
+            })
+            $('.section1 p').css({
+              'margin-left': '-2000px',
+            }).css('transform','rotate(0)');        
+          }, 1000);
+          break;
+        case 2: 
+          setTimeout(function() {
+            $('.section2 h1').css({
+              'transform': 'rotate(0)',
+              'transition': '0.4s'
+            })       
+          }, 1000);
+          break;
+        case 3: 
+          setTimeout(function() {
+            $('.section3 h1').css({
+              'margin-right': '-2000px',
+              'transition': '0.4s'
+            })
+            $('.section3 p').css({
+              'margin-left': '-2000px',
+              'transition': '0.4s'
+            })
+          }, 1000);
+          break;
+        case 4: 
+          setTimeout(function() {
+            $('.column img').css({
+            'transform': 'rotate(0)' 
+            });
+            $('.column h4').css({
+            'transform': 'scale(1)' 
+            });
+          }, 1000);
+          break;
+      }
     },
     'afterRender': function() {
       console.log('页面结构生成后');  
