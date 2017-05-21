afe4183d9407813887452be847b770dce1d82c69
lemontree <gonelove.guo@gmail.com>
2017-05-21 23:57:04 +0800

    增加动画效果

--- js/index.js
+++ js/index.js
@@ -10,6 +10,63 @@ $(function() {
     'navigationTooltips': ['one', 'two', 'three', 'four'], // 定义 导航点文案
     'loopBottom': true,  // 底部之后是否回到顶部 带动画
     'loopHorizontal': false, // 左右滑块是否循环
+    'anchors': ['page1','page2','page3','page4'],
+    'afterLoad': function(link,index) {
+      console.log('加载后',link, index);
+      switch(index) {
+        case 1:
+          $('.section1 h1').css({
+            'transform': 'scale(1.5)',
+            'transition': '0.4s'
+          })
+          $('.section1 p').css({
+            'margin-left': '0',
+            'transition': '0.6s'
+          }).css('transform','rotate(-360deg)');
+          // animate({
+          //   'scale': 1.5
+          // }, "slow");;
+          break;
+        case 2:
+          $('.section2 h1').css({
+            'transform': 'rotate(-360deg)',
+            'transition': '0.4s'
+          })          
+          break;
+        case 3:
+          $('.section3 h1').css({
+            'margin-right': '0',
+            'transition': '0.4s'
+          })
+          $('.section3 p').css({
+            'margin-left': '0',
+            'transition': '0.4s'
+          })
+          break;
+        case 4:
+          $('.column img.primary').animate({
+            'scale': 1.5  
+          }, 'fast', function(){
+            $(this).css({
+              'transition': '0.3s',
+              'transform': 'rotate(360deg)'
+            });
+            $('.column img.primary').animate({
+              'scale': 1.5
+            }, 'fast', function() {
+              
+            });
+          });
+          break;
+      }
+    },
+    'onleave': function(index, nextIndex, direction) {
+      console.log('滚动前', index);  
+    },
+    'afterRender': function() {
+      console.log('页面结构生成后');  
+    }
+    
   });
 
 });
